22caf19f878ae355b7df82157a399e7ae7c2fb7e
mehmeterdemdevge <erdem1devge@gmail.com>
2022-10-01 16:00:21 +0300

    port changed again

--- util/config.js
+++ util/config.js
@@ -2,5 +2,7 @@ require('dotenv').config()
 
 module.exports = {
   DATABASE_URL: process.env.DATABASE_URL,
-  PORT: process.env.http_port|| 3001,
+  PORT: process.env.http_port|| 8080,
 }
+
+console.log(process.env.http_port)
